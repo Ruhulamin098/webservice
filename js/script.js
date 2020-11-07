9e83db95dd2eb9f7580dceff52e95ae918f465d4
Ruhulamin098 <33164298+Ruhulamin098@users.noreply.github.com>
2020-11-08 04:36:36 +0600

    Web Service
    Responsive, UX/UI

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Light Slider
+          $(document).ready(function() {
+        $('#autoWidth').lightSlider({
+            autoWidth:true,
+            loop:true,
+            onSliderLoad: function() {
+                $('#autoWidth').removeClass('cS-hidden');
+            } 
+        });  
+      });
